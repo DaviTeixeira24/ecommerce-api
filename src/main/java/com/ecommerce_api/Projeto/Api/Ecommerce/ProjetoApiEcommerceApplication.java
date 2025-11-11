8c1e8c5232f293f80706f55f15a849e5af79b3dc
DaviTeixeira24 <davisilvateixeiramoreira@gmail.com>
2025-11-11 14:56:14 -0300

    first commit

---
+++ src/main/java/com/ecommerce_api/Projeto/Api/Ecommerce/ProjetoApiEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce_api.Projeto.Api.Ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApiEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApiEcommerceApplication.class, args);
+	}
+
+}
